77ffedf625bff3673c5c6ccf71beeda80577d526
Umer <Umer.Dilpazir@merkle.com>
2023-09-25 12:42:46 +0200

    completed listview

--- shared/src/commonMain/kotlin/App.kt
+++ shared/src/commonMain/kotlin/App.kt
@@ -1,15 +1,10 @@
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -17,39 +12,23 @@ import androidx.compose.foundation.shape.AbsoluteCutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Colors
-import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import dev.icerock.moko.mvvm.compose.getViewModel
 import dev.icerock.moko.mvvm.compose.viewModelFactory
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.client.statement.HttpResponse
-import io.ktor.client.statement.bodyAsChannel
-import io.ktor.http.Headers
-import io.ktor.serialization.kotlinx.json.json
 import model.BirdImage
 import org.jetbrains.compose.resources.ExperimentalResourceApi
-import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun BirdAppTheme(content: @Composable () -> Unit) {
@@ -87,21 +66,18 @@ fun BirdsPage(viewModel: BirdsViewModel) {
             horizontalArrangement = Arrangement.spacedBy(5.dp)
         ) {
             for (category in uiState.categories) {
-                viewModel.selectedCategory(category)
-                Text(text = category)
-
-                /*               Button(
-                                   onClick = {
-                                       viewModel.selectedCategory(category)
-                                   },
-                                   modifier = Modifier.aspectRatio(1.0f).fillMaxWidth().weight(1.0f),
-                                   elevation = ButtonDefaults.elevation(
-                                       defaultElevation = 5.dp,
-                                       focusedElevation = 5.dp
-                                   )
-                               ) {
-                                   Text(category)
-                               }*/
+                Button(
+                    onClick = {
+                        viewModel.selectedCategory(category)
+                    },
+                    modifier = Modifier.aspectRatio(1.0f).fillMaxWidth().weight(1.0f),
+                    elevation = ButtonDefaults.elevation(
+                        defaultElevation = 5.dp,
+                        focusedElevation = 5.dp
+                    )
+                ) {
+                    Text(category)
+                }
             }
         }
         AnimatedVisibility(uiState.categories.isNotEmpty()) {
@@ -111,9 +87,6 @@ fun BirdsPage(viewModel: BirdsViewModel) {
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
                 content = {
-                    item {
-                        Header("it")
-                    }
                     items(uiState.selectedImage) {
                         BirdImageCell(it)
                     }
@@ -123,10 +96,6 @@ fun BirdsPage(viewModel: BirdsViewModel) {
     }
 }
 
-@Composable
-fun Header(categoryName: String) {
-    Text(categoryName)
-}
 
 @Composable
 fun BirdImageCell(image: BirdImage) {
